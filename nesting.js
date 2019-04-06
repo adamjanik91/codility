a7146dd026b8c15a1efd8936267a4fa743fb21de
adamjanik91 <a.janik334@gmail.com>
2019-04-06 17:55:39 +0200

    nesting cd

--- nesting.js
+++ nesting.js
@@ -18,18 +18,54 @@ function solution(S) {
             break;
         }
     }
-    if (hasNesting == false)
+    if (hasNestingChars == false)
         return 1;
 
     var nestingCharArrStart = ["(", "{", "["];
     var nestingCharArrEnd = [")", "}", "]"];
-    var nestingArr = [{ startChar: "(", endChar: ")", startIndexArr: [], EndIndexArr: [] }];
+    var nesting = {
+        '0': { startIndexArr: [], endIndexArr: [] },
+        '1': { startIndexArr: [], endIndexArr: [] },
+        '2': { startIndexArr: [], endIndexArr: [] }
+    };
     for (i = 0; i < S.length; i++) {
         if (nestingCharArr.indexOf(S[i]) != -1) {
-            if (nestingCharArrStart.indexOf(S[i]) != -1) {
-
-            } else if (nestingCharArrEnd.indexOf(S[i]) != -1)
-                nestingArr.push((nestin));
+            var nestingStartIndex = nestingCharArrStart.indexOf(S[i]);
+            var nestingEndIndex = nestingCharArrEnd.indexOf(S[i]);
+            var nestingIndex;
+            if (nestingStartIndex != -1) {
+                nestingIndex = nestingStartIndex;
+                nesting[nestingIndex]['startIndexArr'].push(i);
+            } else if (nestingCharArrEnd.indexOf(S[i]) != -1) {
+                nestingIndex = nestingEndIndex;
+                nesting[nestingIndex]['endIndexArr'].push(i);
+            }
         }
     }
+    var startEndEqual = true;
+    for (key in nesting) {
+        if (nesting[key]['startIndexArr'].length != nesting[key]['endIndexArr'].length)
+            startEndEqual = false;
+    }
+    if (startEndEqual) {
+        var indexToClose = [];
+        for (i = 0; i < S.length; i++) {
+            if (nestingCharArrEnd.indexOf(S[i]) != -1) {
+                if (indexToClose.length == 0)
+                    return 0;
+                var closingIndexType = nestingCharArrEnd.indexOf(S[i]);
+                if (nestingCharArrStart.indexOf(S[indexToClose.length - 1]) == closingIndexType) {
+                    indexToClose.pop();
+                }
+            }
+            if (indexToClose[indexToClose.length - 1])
+                var startIndex = nestingCharArrStart.indexOf(S[i]);
+            if (startIndex != -1)
+                indexToClose.push(i);
+        }
+        if (indexToClose.length == 0)
+            return 1;
+    }
+
+    return 0;
 }
